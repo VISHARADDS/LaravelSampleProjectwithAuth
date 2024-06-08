6448efe4e98262f5df4b1270ed29bfdbc5a8cd22
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-09 00:40:12 +0530

    adding image colum to db

--- sampleproject-app/database/migrations/2024_06_08_190218_add_image_to_products_table.php
+++ sampleproject-app/database/migrations/2024_06_08_190218_add_image_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('size');
+            $table->string('photo')->nullable()->after('description');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('size');
+           //
         });
     }
 };
