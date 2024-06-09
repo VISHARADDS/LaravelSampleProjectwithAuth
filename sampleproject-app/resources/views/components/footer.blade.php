f1507c5dd62fbd3b6470a9611e676f05152f0c40
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-09 19:11:50 +0530

    footer updates

---
+++ sampleproject-app/resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<div>
+  <footer class="footer mt-auto py-3" style="bottom: 0; width: 100%; background-color: black; position: static; box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);">
+    <div class="container" style="padding-top: 20px;">
+      <div class="row text-center text-white">
+        <div class="col-md-4 mx-auto">
+          <p class="mb-3" style="margin-left:80px">All Rights Reserved for © <b>TECH DEAL</b></p>
+          <ul class="list-unstyled">
+            <li style="margin-left:80px"><a href="#" class="text-white">About Us</a></li>
+            <li style="margin-left:80px"><a href="#" class="text-white">FAQ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
