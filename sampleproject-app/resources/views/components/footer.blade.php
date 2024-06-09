665e6082e11a338a8692d5c236f19581e0229378
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-09 21:30:26 +0530

    footer adjustments

--- sampleproject-app/resources/views/components/footer.blade.php
+++ sampleproject-app/resources/views/components/footer.blade.php
@@ -3,10 +3,10 @@
     <div class="container" style="padding-top: 20px;">
       <div class="row text-center text-white">
         <div class="col-md-4 mx-auto">
-          <p class="mb-3" style="margin-left:80px">All Rights Reserved for © <b>TECH DEAL</b></p>
+          <p class="mb-3" style="margin-left:65px">All Rights Reserved for © <b>TECH DEAL</b></p>
           <ul class="list-unstyled">
-            <li style="margin-left:80px"><a href="#" class="text-white">About Us</a></li>
-            <li style="margin-left:80px"><a href="#" class="text-white">FAQ</a></li>
+            <li style="margin-left:60px"><a href="#" class="text-white">About Us</a></li>
+            <li style="margin-left:60px"><a href="#" class="text-white">FAQ</a></li>
           </ul>
         </div>
       </div>
