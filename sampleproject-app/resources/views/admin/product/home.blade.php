2e9419c05aaebe8128cde2b47e71e1486e240783
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-09 15:43:07 +0530

    welcome bage chnages

--- sampleproject-app/resources/views/admin/product/home.blade.php
+++ sampleproject-app/resources/views/admin/product/home.blade.php
@@ -1,6 +1,23 @@
 <x-app-layout>
 @push('styles')
         <link rel="stylesheet" href="{{ asset('css/headerUI.css') }}"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <style>
+        .showPhoto{
+            width:50%;
+            height:50px;
+        }
+        .showPhoto>div{
+            width:100%;
+            height:100%;
+            margin:auto;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        </style>   
     @endpush
    
 
@@ -16,7 +33,7 @@
                 <div class="card my-4">
                     <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3">PRODUCTS</h6>
+                            <h6 class="text-white text-capitalize ps-3"><b>PRODUCTS</b></h6>
                         </div>
                     </div>
                     <div class="card-body px-0 pb-2">
@@ -42,7 +59,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                 @forelse ($products as $product)
+                                 @forelse ($products as $product )
                                    <tr >
                                   
                                         <td>
@@ -55,7 +72,7 @@
                                         <td>
                                             <h6 style="color:black" class="mb-0 text-sm">
                                                 <div class="showPhoto">
-                                                    <div id="imagePreview" if >
+                                                    <div id="imagePreview" style="@if ($product->photo !='') background-image:url('{{url('/')}}/uploads/{{ $product->photo}}')@else background-image: url('{{url('/image/avatar.png')}}') @endif;" >
                                                 </div>
                                               </div>    
                                             </h6>
