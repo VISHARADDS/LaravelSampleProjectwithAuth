0056e0b4dd8c2b123a1f4739ad29c0d38242f149
VISHARADDS <118560818+VISHARADDS@users.noreply.github.com>
2024-06-09 21:16:24 +0530

    css for login reg form adjustments

--- sampleproject-app/resources/views/profile/edit.blade.php
+++ sampleproject-app/resources/views/profile/edit.blade.php
@@ -1,11 +1,15 @@
 <x-app-layout>
+@push('styles')
+        <link rel="stylesheet" href="{{ asset('css/headerUI.css') }}"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+    @endpush
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div style="margin-top:2rem;">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
